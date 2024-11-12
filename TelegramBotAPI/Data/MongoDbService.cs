2d0df14773057a3a1ebcd6589364d56f0308d544
NZ <nikita90103@mail.ru>
2024-11-12 17:27:16 +0500

    Add Entity, MongoDbService and Controller by TelegramBot

---
+++ TelegramBotAPI/Data/MongoDbService.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using System.Configuration;
+
+namespace TelegramBotAPI.Data
+
+{
+    public class MongoDbService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase? _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            var connectionString = _configuration.GetConnectionString("DbConnection");
+            var mongoUrl = MongoUrl.Create(connectionString);
+            var mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        public IMongoDatabase? Database => _database;
+    }
+}
--- TelegramBotAPI/Program.cs
+++ TelegramBotAPI/Program.cs
@@ -1,12 +1,11 @@
+using TelegramBotAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<MongoDbService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
